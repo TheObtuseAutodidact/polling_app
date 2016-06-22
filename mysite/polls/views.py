f0a1c5c1a0b334ee3c02419d41be21feeb9fc683
Obtuse <theobtuseautodidact@gmail.com>
2016-06-22 13:01:20 -0700

    complete django beginner tutorial part 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,44 @@
 # from django.shortcuts import render  ### this was a default import
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+
+# Note that once we’ve done this in all these views, we no longer need to import loader and HttpResponse (you’ll want to keep HttpResponse if you still have the stub methods for detail, results, and vote).
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+    ### Shorter Rewrite ###
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    context = { 'latest_question_list': latest_question_list }
+    return render(request, 'polls/index.html', context)
+
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)  #pre-loader
+
+def detail(request, question_id):
+    # return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', { 'question': question })
+    #### Rewrite
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', { 'question': question })
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
